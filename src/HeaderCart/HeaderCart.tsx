e4cce52cbed809721336e551b29c4af28d663614
Andrei Dzilanian <andrei.dzilanian@gmail.com>
2023-12-24 16:41:59 +0100

    fixed bug in header

--- src/HeaderCart/HeaderCart.tsx
+++ src/HeaderCart/HeaderCart.tsx
@@ -1,27 +1,22 @@
-import React, { useState } from "react";
+import { FC } from "react";
 import styles from "./HeaderCart.module.scss";
 import CartIcon from "../CartIcon";
 
 type CartButtonProps = {
   itemsCount: number;
   onOpenModal: (isOpen: boolean) => void;
- 
 };
 
-const CartButton: React.FC<CartButtonProps> = ({ itemsCount, onOpenModal }) => {
-
-
+const CartButton: FC<CartButtonProps> = ({ itemsCount, onOpenModal }) => {
   const openModalHandler = () => {
-    onOpenModal(true)
-  }
-
-  const itemProof = itemsCount < 0 ? 0 : itemsCount;
+    onOpenModal(true);
+  };
 
   return (
     <button className={styles.button} onClick={openModalHandler}>
       <CartIcon />
       Warenkorb
-      <span className={styles.badge}>{itemProof}</span>
+      <span className={styles.badge}>{itemsCount < 0 ? 0 : itemsCount}</span>
     </button>
   );
 };
